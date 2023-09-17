949b4b2c989b609cd29040fbd90b70a3548cce4b
Oakkar Phyo <oakkarphyo7@gmail.com>
2023-09-17 18:18:17 +0630

    Add design options

--- appengine/flexible/qr_code_generator/main.py
+++ appengine/flexible/qr_code_generator/main.py
@@ -3,23 +3,34 @@ import qrcode
 from io import BytesIO
 from base64 import b64encode
 from qrcode.image.styledpil import StyledPilImage
-from qrcode.image.styles.moduledrawers.pil import GappedSquareModuleDrawer, RoundedModuleDrawer, CircleModuleDrawer, SquareModuleDrawer, VerticalBarsDrawer, HorizontalBarsDrawer
+from qrcode.image.styles.moduledrawers.pil import (
+    SquareModuleDrawer,
+    GappedSquareModuleDrawer,
+    RoundedModuleDrawer,
+    CircleModuleDrawer,
+    VerticalBarsDrawer,
+    HorizontalBarsDrawer,
+)
 
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/', methods=['POST'])
+
+@app.route("/", methods=["POST"])
 def generateQR():
-
     # Initiate BytesIO stream
     memory = BytesIO()
 
     # Get data from form
-    data = request.form.get('link')
+    data = request.form.get("link")
+
+    # Get design selection from form
+    design = request.form.get("design")
 
     # Generate QR Code
     qr = qrcode.QRCode(error_correction=qrcode.constants.ERROR_CORRECT_L)
@@ -29,15 +40,41 @@ def generateQR():
     qr.make(fit=True)
 
     # Create an image from the QR Code instance
-    img = qr.make_image(image_factory=StyledPilImage, module_drawer=GappedSquareModuleDrawer())
+    if design == "design-1":
+        img = qr.make_image(
+            image_factory=StyledPilImage, module_drawer=SquareModuleDrawer()
+        )
+    elif design == "design-2":
+        img = qr.make_image(
+            image_factory=StyledPilImage, module_drawer=GappedSquareModuleDrawer()
+        )
+    elif design == "design-3":
+        img = qr.make_image(
+            image_factory=StyledPilImage, module_drawer=RoundedModuleDrawer()
+        )
+    elif design == "design-4":
+        img = qr.make_image(
+            image_factory=StyledPilImage, module_drawer=CircleModuleDrawer()
+        )
+    elif design == "design-5":
+        img = qr.make_image(
+            image_factory=StyledPilImage, module_drawer=VerticalBarsDrawer()
+        )
+    elif design == "design-6":
+        img = qr.make_image(
+            image_factory=StyledPilImage, module_drawer=HorizontalBarsDrawer()
+        )
 
-    img.save(memory, 'PNG')
+
+    img.save(memory, "PNG")
     memory.seek(0)
 
-    base64_img = "data:image/png;base64," + b64encode(memory.getvalue()).decode('ascii')
-    text = "QR Code Generated Successfully!😎"
+    base64_img = "data:image/png;base64," + b64encode(memory.getvalue()).decode("ascii")
+    text = f"QR code generated successfully for {data}!🥳"
+    selected = "Design Selected: " + design
 
-    return render_template('index.html', data=base64_img, text=text)
+    return render_template("index.html", qr_img=base64_img, text=text)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
